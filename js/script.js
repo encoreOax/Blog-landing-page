ff2c0ade7e65e69d994c318194d1872920b3f550
Hector Sánchez Coronel <82076823+encoreOax@users.noreply.github.com>
2021-09-17 01:30:05 -0500

    Mobile versión finish
    The mobile version is completed as requested, now media-query for tamblet and desktop version

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const btnHam = document.getElementById('ham')
+const navMobile = document.getElementById('nav')
+const subMenuBtn = document.querySelectorAll('.sub-menu-btn')
+
+btnHam.addEventListener('click', ()=>{
+    console.log('click')
+    if (btnHam.classList.contains('ham-on')){
+        btnHam.classList.remove('ham-on')
+        btnHam.classList.add('ham-off')
+        navMobile.classList.remove('vis-hidden')
+        navMobile.classList.add('vis-visible')
+    }else {
+        btnHam.classList.remove('ham-off')
+        btnHam.classList.add('ham-on')
+        navMobile.classList.remove('vis-visible')
+        navMobile.classList.add('vis-hidden')
+        
+    }
+})
+for (let index = 0; index < subMenuBtn.length; index++) {
+    subMenuBtn[index].addEventListener('click', ()=> {
+        const subMenu = subMenuBtn[index].nextElementSibling
+        const navIcon = subMenuBtn[index].firstElementChild
+        console.log('working')
+        if (subMenu.style.display == ('block')) {
+            navIcon.classList.remove('nav_icon-open')
+            navIcon.classList.add('nav_icon')
+            subMenuBtn[index].style.color = ('var(--primary-very-dark-blue-heading)')
+            subMenu.style.display = ('none')
+            
+        } else {
+            navIcon.classList.remove('nav_icon')
+            navIcon.classList.add('nav_icon-open')
+            subMenuBtn[index].style.color = ('var(--neutral-very-dark-grayish-blue-body-copy)')
+            subMenu.style.display = ('block')
+            
+        }
+    })
+    
+}
