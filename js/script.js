835b8a0d0eb115602ba5571092a7da371bd864b4
Hector Sánchez Coronel <82076823+encoreOax@users.noreply.github.com>
2021-09-18 02:48:28 -0500

    Finish desktop version
    Need some ajustments and animations

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const btnHam = document.getElementById('ham')
 const navMobile = document.getElementById('nav')
 const subMenuBtn = document.querySelectorAll('.sub-menu-btn')
+const subMenuBtnDesk = document.querySelectorAll('.sub-menu-btn-desk')
 
 btnHam.addEventListener('click', ()=>{
-    console.log('click')
+
     if (btnHam.classList.contains('ham-on')){
         btnHam.classList.remove('ham-on')
         btnHam.classList.add('ham-off')
@@ -21,7 +22,7 @@ for (let index = 0; index < subMenuBtn.length; index++) {
     subMenuBtn[index].addEventListener('click', ()=> {
         const subMenu = subMenuBtn[index].nextElementSibling
         const navIcon = subMenuBtn[index].firstElementChild
-        console.log('working')
+
         if (subMenu.style.display == ('block')) {
             navIcon.classList.remove('nav_icon-open')
             navIcon.classList.add('nav_icon')
@@ -33,6 +34,27 @@ for (let index = 0; index < subMenuBtn.length; index++) {
             navIcon.classList.add('nav_icon-open')
             subMenuBtn[index].style.color = ('var(--neutral-very-dark-grayish-blue-body-copy)')
             subMenu.style.display = ('block')
+            
+        }
+    })
+    
+}
+for (let index = 0; index < subMenuBtnDesk.length; index++) {
+    subMenuBtnDesk[index].addEventListener('click', ()=> {
+        const subMenu = subMenuBtnDesk[index].nextElementSibling
+        const navIcon = subMenuBtnDesk[index].firstElementChild
+      
+        if (subMenu.style.display == ('flex')) {
+            navIcon.classList.remove('nav_icon-open')
+            navIcon.classList.add('nav_icon')
+            subMenu.style.display = ('none')
+            subMenu.classList.remove('vis-hidden')
+            subMenu.classList.add('vis-visible')
+            
+        } else {
+            navIcon.classList.remove('nav_icon')
+            navIcon.classList.add('nav_icon-open')
+            subMenu.style.display = ('flex')
             
         }
     })
